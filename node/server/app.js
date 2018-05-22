4f9c60a9391f4310020ec280480cf97ec44dbc91
mfosse <matt.cfosse@gmail.com>
2018-05-22 13:03:44 +0000

    add lagless audio

--- node/server/app.js
+++ node/server/app.js
@@ -376,6 +376,15 @@ function getImageFromUser3(user, x1, y1, x2, y2, quality, scale) {
 
 //var numClients = 0;
 
+var channels = {};
+
+io.set('transports', [
+	'polling',
+    'websocket',
+    'xhr-polling',
+    'jsonp-polling'
+]);
+
 io.on('connection', function(socket) {
 
 	//console.log("USER CONNECTED");
@@ -608,10 +617,69 @@ io.on('connection', function(socket) {
 // 		streamSettings.y1 = data.y1 || streamSettings.y1;
 //     streamSettings.y2 = data.y2 || streamSettings.y2;
 // 	});
+	
+	
+	
+	/* WebRTC @@@@@@@@@@@@@@@@@@@@@@@@ */
+	
+	
+
+    socket.on("message", function (data) {
+        socket.broadcast.emit("message", data);
+    });
+	
+    var initiatorChannel = '';
+    if (!io.isConnected) {
+        io.isConnected = true;
+    }
+	
+    socket.on('new-channel', function (data) {
+        if (!channels[data.channel]) {
+            initiatorChannel = data.channel;
+        }
+
+        channels[data.channel] = data.channel;
+        onNewNamespace(data.channel, data.sender);
+    });
+
+    socket.on('presence', function (channel) {
+        var isChannelPresent = !! channels[channel];
+        socket.emit('presence', isChannelPresent);
+    });
+
+    socket.on('disconnect', function (channel) {
+        if (initiatorChannel) {
+            delete channels[initiatorChannel];
+        }
+    });
 
 });
 
+function onNewNamespace(channel, sender) {
+    io.of('/' + channel).on('connection', function (socket) {
+        var username;
+        if (io.isConnected) {
+            io.isConnected = false;
+            socket.emit('connect', true);
+        }
 
+        socket.on('message', function (data) {
+            if (data.sender == sender) {
+                if(!username) username = data.data.sender;
+				console.log(data);
+                
+                socket.broadcast.emit('message', data.data);
+            }
+        });
+        
+        socket.on('disconnect', function() {
+            if(username) {
+                socket.broadcast.emit('user-left', username);
+                username = null;
+            }
+        });
+    });
+}
 
 // 	setInterval(function(){
 // 		var user = "Matt";
